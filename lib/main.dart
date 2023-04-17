771b80cb78c70b2e95e91705dbff17464ffc9eae
ArishSiddiqui <siddiquiarish100@gmail.com>
2023-04-17 14:51:27 +0530

    Completed Task

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: OtpPage(mobNum: '7700036298'),
+      home: LoginPage(),
     );
   }
 }
